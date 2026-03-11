a4a327cdf3db14d27cedf33ed8bec07fddda7379
Un1ucky0n3 <pawel.gonto@gmail.com>
2026-03-11 09:47:29 +0100

    feature-conflict conflict simulation

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -22,4 +22,4 @@ static double CalculateMin(double x, double y)
 {
     return Math.Min(x, y);
 }
-Console.WriteLine("Calculate Min Created!!!");
+Console.WriteLine("feature-conflict conflict simulation!!!");
